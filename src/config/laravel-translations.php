be44534f1860beee18d4d6508598211fb307d851
Ebrahim-Hanna-Sync <ebrahimhanna@wecansync.com>
2024-10-25 15:09:00 +0300

    Update fields description for more clarity

--- src/config/laravel-translations.php
+++ src/config/laravel-translations.php
@@ -8,12 +8,15 @@ return [
     'language_model' => 'App\Models\Language',
 
     /*
-     * Name of the relationship key between the language model and where you gonna store the translated data
+     * Foreign key referencing the 'languages' table.
+     * This field indicates the language in which the translation is written.
      */
     'foreign_key' => 'language_id',
 
     /*
-     * Name of the relationship key between the main model and where you gonna store the translated data
+     * Foreign key referencing another table.
+     * This field links the translation to a specific record in another table,
+     * allowing for multiple translations for that record.
      */
     'owner_key' => 'model_id',
 ];
